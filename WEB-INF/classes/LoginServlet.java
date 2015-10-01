d59941ccc4023a51e451e3834ac0424c7fb53cc3
sameersk123 <sksameersk143@gmail.com>
2015-10-01 15:05:58 +0530

    login

--- WEB-INF/classes/LoginServlet.java
+++ WEB-INF/classes/LoginServlet.java
@@ -1,20 +1,14 @@
 import java.sql.*;
 import java.io.*;
-import javax.servlet.http.*;
-public class LoginServlet extends HttpServlet
+import javax.servlet.*;
+public class LoginServlet extends GenericServlet
 {
 	Connection conn;
-	
-	public void doGet(HttpServletRequest hreq,HttpServletResponse hres)
+	public void service(ServletRequest req,ServletResponse res)
 	{
-	
 		try{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1523:xe","system","manager");
-		
-	
-	
-	
 		String s1=req.getParameter("uname");
 		String s2=req.getParameter("pword");
 		PreparedStatement pstmt=conn.prepareStatement("select * from uinfo where uname=? and pword=?");
@@ -23,6 +17,7 @@ public class LoginServlet extends HttpServlet
 		ResultSet rs=pstmt.executeQuery();
 		PrintWriter pw=res.getWriter();
 		pw.println("<html><body bgcolor=red text=black><h1>");
+		ServletContext sc=getServletContext();
 		if(rs.next())
 		{
 			pw.println("You have Successfully Logged in");
@@ -30,6 +25,8 @@ public class LoginServlet extends HttpServlet
 		else
 		{
 			pw.println("Invalid uname/pword");
+			RequestDispatcher rd=sc.getRequestDispatcher("/login.html");
+			rd.include(req,res);
 		}
 		pw.println("</h1></body></html>");
 							
--- WEB-INF/classes/RegServlet.java
+++ WEB-INF/classes/RegServlet.java
@@ -1,28 +1,25 @@
 import java.sql.*;
 import java.io.*;
-import javax.servlet.http.*;
-public class RegServlet extends HttpServlet
+import javax.servlet.*;
+public class RegServlet extends GenericServlet
 {
 	Connection conn;
-		public void doGet(HttpServletRequest hreq,HttpServletResponse hres)
+		public void service(ServletRequest req,ServletResponse res)
 	{
 		try{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		conn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1523:xe","system","manager");
-		
-
-	
-		String s1=hreq.getParameter("fname");
-		String s2=hreq.getParameter("lname");
-		String s3=hreq.getParameter("uname");
-		String s4=hreq.getParameter("pword");
-		PreparedStatement pstmt=conn.prepareStatement("insert into table uinfo values(?,?,?,?)");
+		String s1=req.getParameter("fname");
+		String s2=req.getParameter("lname");
+		String s3=req.getParameter("uname");
+		String s4=req.getParameter("pword");
+		PreparedStatement pstmt=conn.prepareStatement("insert into uinfo values(?,?,?,?)");
 		pstmt.setString(1,s1);
 		pstmt.setString(2,s2);
 		pstmt.setString(3,s3);
 		pstmt.setString(4,s4);
 		pstmt.executeUpdate();
-		PrintWriter pw=hres.getWriter();
+		PrintWriter pw=res.getWriter();
 		pw.println("<html><body bgcolor=green text=wheat><center><h1>Registration Sucessfull</h1><br>");
 		pw.println("<a href=login.html>Login</a></center></body></html>");
 	}catch(Exception e)
